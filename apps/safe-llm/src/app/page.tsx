d95d4d13f5ce3e191f0447b009ca82d4c721a7ff
Dhruv <malikdhruv1994@gmail.com>
2024-06-13 23:22:13 +0200

    init(WIP):
    - adding additional shadcn components
    - changes in the page layout with next-themes

--- apps/safe-llm/src/app/page.tsx
+++ apps/safe-llm/src/app/page.tsx
@@ -1,8 +1,9 @@
 
 
-import { Button } from '../../../../shared/src/components/ui/button';
+import { Button } from '@nx-nextjs-llm/shared';
+import Link from "next/link"
 
-export default async function Index() {
+export default async function Index(): JSX.IntrinsicElements {
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen bg-slate-600">
       <h1 className="text-6xl text-white">Hello World</h1>
